a917ea3ece5f690f75c3ee3ce75dcafddf4279e4
Morris Hannessen <morris.hannessen@gmail.com>
2025-06-18 14:41:09 +0200

    Fix JSON circular reference causing 500 errors - Add JsonIgnore to UserExtraData.User navigation property

--- babbly-user-service/Models/UserExtraData.cs
+++ babbly-user-service/Models/UserExtraData.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace babbly_user_service.Models
 {
@@ -26,8 +27,9 @@ namespace babbly_user_service.Models
 
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
-        // Navigation property
+        // Navigation property - JsonIgnore to prevent circular reference
         [ForeignKey("UserId")]
+        [JsonIgnore]
         public User User { get; set; } = null!;
     }
 } 
